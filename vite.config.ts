31860f477514ffd7d5e470a4c8a0b93d159eefe5
Iris <140817611+irisihrz@users.noreply.github.com>
2025-04-03 14:19:07 +0200

    fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/fhsdata/' : '/',
+export default defineConfig({
+  base: '/',
   build: {
     outDir: "dist",
     assetsDir: "assets",
@@ -21,4 +21,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
